ee1a003f6bcd4926071d809f54e3bfd6e6eaa6be
Mperonneau <peronneaumoliere@gmail.com>
2021-03-04 15:11:18 -0500

    Update settings.py

--- pulucia/settings.py
+++ pulucia/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '05yt^+j8snkvtutmmc=ox)v&2_qk(%7pv)-@3%o5ntz5^tfvp8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pulucia.herokuapp.com', '127.0.0.1','192.168.1.4', 'localhost', 'www.editionspulucia.com'] #192.168.1.5: test the web on phone....
+ALLOWED_HOSTS = ['pulucia.herokuapp.com',  'www.editionspulucia.com'] #192.168.1.5: test the web on phone....
 
 
 # Application definition
@@ -69,10 +69,10 @@ MIDDLEWARE = [
 #DBBACKUP_STORAGE_OPTIONS = {'location': '/Users/mperonneau/Desktop/dev_site/backup'}
 
 #Dropbox
-DBBACKUP_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
-DBBACKUP_STORAGE_OPTIONS = {
-    'oauth2_access_token': 'X_aTWNofZtkAAAAAAAAAAexU3LaDxk2IOajdguclZAXw_rtMimigxg0dgqtGNFw0',
-}
+#DBBACKUP_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
+#DBBACKUP_STORAGE_OPTIONS = {
+    #'oauth2_access_token': 'X_aTWNofZtkAAAAAAAAAAexU3LaDxk2IOajdguclZAXw_rtMimigxg0dgqtGNFw0',
+#}
 
 
 
